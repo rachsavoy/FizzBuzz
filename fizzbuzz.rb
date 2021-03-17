37c512509af2cb5762975fefcb2bee1161cb5b4c
Rachael Savoy <rachael.a.hakim@gmail.com>
2021-03-17 15:16:30 +0900

    FIX: refactored code. DRY.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,21 +1,10 @@
-def fizzbuzz(number)
-  return "Fizz" if divisible_by_three?(number)
-  return "Buzz" if divisible_by_five?(number)
-  return "FizzBuzz" if divisible_by_both?(number)
+1.upto(100) do |i|
+  string = ""
+
+  string += "Fizz" if i % 3 == 0
+  string += "Buzz" if i % 5 == 0
+  string += i if string.blank?
+
+  puts string
 end
 
-def divisible_by(number, divisor)
-  number % divisor == 0
-end
-
-def divisible_by_three(number)
-  divisible_by(number, 3)
-end
-
-def divisible_by_five(number)
-  divisible_by(number, 5)
-end
-
-def divisible_by_both(number)
-  divisible_by(number, 15)
-end
